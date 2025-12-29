180c52222b025f8488b8a9c38f13da6161dd82b7
Frank Hilgenberg <frank.hilgenberg@franks-divecenter.de>
2025-12-29 18:11:53 +0100

    Ignore local config.php and add config example

---
+++ config/config.example.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+return [
+  'db' => [
+    'host' => '127.0.0.1',
+    'name' => 'database_name',
+    'user' => 'username',
+    'pass' => 'password',
+    'charset' => 'utf8mb4',
+  ],
+];
